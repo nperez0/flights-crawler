d9d07e64c36301e5a03fc01d4b56d9b0b245f394
Nicolas Perez Ortiz <perezn@ryanair.com>
2026-01-12 15:34:52 +0100

    Avoid stopping the whole app for errors in queries

--- src/Flights.Data/Models/Result/FlightQueryResult.cs
+++ src/Flights.Data/Models/Result/FlightQueryResult.cs
@@ -1,9 +1,17 @@
-﻿using Flights.Data.Models.Query;
-
-namespace Flights.Data.Models.Result;
+﻿namespace Flights.Data.Models.Result;
 
 public class FlightQueryResult
 {
+    public static FlightQueryResult Empty => new()
+    {
+        Id = Guid.Empty,
+        QueryId = Guid.Empty,
+        Provider = string.Empty,
+        Solutions = [],
+        TotalSolutionCount = 0,
+        SearchedAt = DateTime.MinValue
+    };
+
     public required Guid Id { get; set; }
 
     public required Guid QueryId { get; set; }
